34efc54e7ac4b051b9745fd361ffc67cb823c94b
Heaveness <44594469+Heaveness@users.noreply.github.com>
2026-04-12 21:39:12 -0400

    Initial Release

---
+++ Patch.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using Player;
+
+namespace InfectionDeathPenalty;
+
+[HarmonyPatch]
+internal static class Patch
+{
+    [HarmonyPatch(typeof(Dam_PlayerDamageBase), nameof(Dam_PlayerDamageBase.ReceiveSetDead))]
+    [HarmonyPrefix]
+    public static void OnPlayerDown(Dam_PlayerDamageBase __instance)
+    {
+        PlayerAgent player = __instance.Owner;
+        if (player == null) return;
+
+        if (!player.Alive) return;
+
+        if (player.Owner.IsBot) return;
+        if (!player.IsLocallyOwned) return;
+
+        float currentInf = __instance.Infection;
+        float maxAllowed = 0.85f;
+
+        if (currentInf >= maxAllowed) return;
+
+        float amountToAdd = Plugin.InfectionPercentage.Value / 100f;
+
+        float newInfection = Math.Min(currentInf + amountToAdd, maxAllowed);
+
+        pInfection infectionData = new()
+        {
+            amount = newInfection,
+            mode = pInfectionMode.Set
+        };
+
+        __instance.ModifyInfection(infectionData, true, true);
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using GTFO.API.Utilities;
+
+namespace InfectionDeathPenalty;
+
+[BepInPlugin("Heaveness.InfectionDeathPenalty", "InfectionDeathPenalty", "1.0.0")]
+[BepInDependency("dev.gtfomodding.gtfo-api")]
+public class Plugin : BasePlugin
+{
+    public static ConfigEntry<float> InfectionPercentage;
+
+    public override void Load()
+    {
+        InfectionPercentage = Config.Bind(
+            "Settings",
+            "InfectionPercentage",
+            10f,
+            new ConfigDescription(
+                "Infection percent to apply when a player goes down. Does accept decimal values.",
+                new AcceptableValueRange<float>(0f, 85f)
+            )
+        );
+
+        Log.LogInfo("InfectionDeathPenalty is loaded!");
+        Log.LogInfo($"Infection percentage set to: {InfectionPercentage.Value}%");
+
+        var listener = LiveEdit.CreateListener(Paths.ConfigPath, Path.GetFileName(Config.ConfigFilePath), false);
+        listener.FileChangedEventCooldown = 0.75f;
+        listener.FileChanged += _ =>
+        {
+            Config.Reload();
+            Log.LogInfo("LiveEdit: InfectionDeathPenalty Configuration reloaded");
+        };
+
+        new Harmony("Heaveness.InfectionDeathPenalty").PatchAll();
+    }
+}
